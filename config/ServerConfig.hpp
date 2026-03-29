08861bb96e0a839546bb9459722896aafa6a8e19
pafroidu <pafroidu@student.42.fr>
2026-03-29 17:29:35 +0200

    feat: add ServerConfig .hpp w/ full OCF and getter + setters + add methods
    - full .hpp file defining the SERVER CONFIG class
    - OCF form
    - private members for all the ServerConfig fields
    - getter + setters + add methods for all fields
    - file formatted with clang-format Google cpp style

---
+++ config/ServerConfig.hpp
@@ -0,0 +1,41 @@
+#ifndef SERVER_CONFIG_HPP
+#define SERVER_CONFIG_HPP
+
+#include <map>
+#include <string>
+#include <vector>
+
+#include "LocationConfig.hpp"
+
+class ServerConfig {
+private:
+    std::string host_;
+    int port_;
+    size_t max_body_size_;
+    std::map<int, std::string> error_pages_;
+    std::vector<LocationConfig> location_block_;
+
+public:
+    ServerConfig();
+    ServerConfig(const ServerConfig& copy);
+    ServerConfig& operator=(const ServerConfig& other);
+    ~ServerConfig();
+
+    // getters
+    const std::string& getHost() const;
+    int getPort() const;
+    size_t getMaxBodySize() const;
+    const std::map<int, std::string>& getErrorPages() const;
+    const std::vector<LocationConfig>& getLocationBlock() const;
+
+    // setters
+    void setHost(const std::string& host);
+    void setPort(int port);
+    void setMaxBodySize(size_t max_body_size);
+    // error_page 404 /errors/404.html; (one entry at a time)
+    void addErrorPage(int code, const std::string& path);
+    // location /upload { ... } (one block at a time)
+    void addLocationBlock(const LocationConfig& location);
+};
+
+#endif
