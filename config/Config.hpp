884aa0bf191bb3afbf8c8ff04505e84c2dcc8d63
pafroidu <pafroidu@student.42.fr>
2026-03-29 15:58:15 +0200

    feat: add Config .hpp w/ full OCF and getter + add methods
    - full .hpp file defining the CONFIG class
    - OCF form
    - private member vector of ServerConfig
    - getter + add method for the vector of ServerConfig (no setter needed
    with a vector, we need to add an item to the vector one by one as we
    parse them, not replace every item at once)
    - file formatted with clang-format Google cpp style (I'll need time to
    get used to it, it's ugly :((((( )

---
+++ config/Config.hpp
@@ -0,0 +1,22 @@
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
+
+#include <vector>
+
+#include "ServerConfig.hpp"
+
+class Config {
+private:
+    std::vector<ServerConfig> server_block_;
+
+public:
+    Config();
+    Config(const Config& copy);
+    Config& operator=(const Config& other);
+    ~Config();
+
+    const std::vector<ServerConfig>& getServerBlock() const;
+    void addServerBlock(const ServerConfig& server);
+};
+
+#endif
