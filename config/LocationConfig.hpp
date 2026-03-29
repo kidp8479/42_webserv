95ce7b1819025f1d0006db282a6d3b7352325c94
pafroidu <pafroidu@student.42.fr>
2026-03-29 17:03:59 +0200

    feat: add LocationConfig .hpp w/ full OCF and getter + setters + add methods
    - full .hpp file defining the LOCATION CONFIG class
    - OCF form
    - private members for all the LocationConfig fields
    - getter + setters + add methods for all fields
    - file formatted with clang-format Google cpp style

---
+++ config/LocationConfig.hpp
@@ -0,0 +1,50 @@
+#ifndef LOCATION_CONFIG_HPP
+#define LOCATION_CONFIG_HPP
+
+#include <string>
+#include <vector>
+
+#define NO_REDIRECT -1
+
+class Location {
+private:
+    std::string path_;
+    std::vector<std::string> methods_;
+    std::string root_;
+    std::string index_;
+    bool directory_listing_;
+    std::string upload_path_;
+    std::vector<std::string> cgi_extensions_;
+    int return_code_;  // NO_REDIRECT (-1) if no return directive is set
+    std::string return_url_;
+
+public:
+    Location();
+    Location(const Location& copy);
+    Location& operator=(const Location& other);
+    ~Location();
+
+    // getters
+    const std::string& getPath() const;
+    const std::vector<std::string>& getMethods() const;
+    const std::string& getRoot() const;
+    const std::string& getIndex() const;
+    bool getDirectoryListing() const;
+    const std::string& getUploadPath() const;
+    const std::vector<std::string>& getCgiExtensions() const;
+    int getReturnCode() const;
+    const std::string& getReturnUrl() const;
+
+    // setters
+    void setPath(const std::string& path);
+    void setMethods(const std::vector<std::string>& methods);
+    void setRoot(const std::string& root);
+    void setIndex(const std::string& index);
+    void setDirectoryListing(bool directory_listing);
+    void setUploadPath(const std::string& upload_path);
+    void setCgiExtensions(const std::vector<std::string>& cgi_extensions);
+    void setReturnCode(int return_code);
+    void setReturnUrl(const std::string& return_url);
+};
+
+#endif
