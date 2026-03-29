8a3670c1904f674b674e718dc6b4f5abad4e880b
pafroidu <pafroidu@student.42.fr>
2026-03-29 17:33:52 +0200

    chore: placeholder for ConfigParser
    this will be coded later, this is the parsing motor that will parse and
    validate data containers Config LocationConfig and ServerConfig

---
+++ config/ConfigParser.cpp
@@ -0,0 +1,4 @@
+#include "ConfigParser.hpp"
+
+// TO BE CODED WHEN I COME BACK
+// this is the heart of the parsing
---
+++ config/ConfigParser.hpp
@@ -0,0 +1,16 @@
+#ifndef CONFIG_PARSER_HPP
+#define CONFIG_PARSER_HPP
+
+// TO BE CODED WHEN I COME BACK
+// this is the heart of the parsing
+
+class ConfigParser {
+private:
+public:
+    ConfigParser();
+    ConfigParser(const ConfigParser& copy);
+    ConfigParser& operator=(const ConfigParser& other);
+    ~ConfigParser();
+};
+
+#endif
