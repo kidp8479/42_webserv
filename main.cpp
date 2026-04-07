4ecc234840755d3ca0e58c819fdcba82698921fd
d2 <d2.de.sousa@gmail.com>
2026-04-07 15:28:14 +0200

    feat: bootstrap logger in main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
-// hello, I'm a main palceholder, I do absolutely nothing for now :)
+#include "logger/Logger.hpp"
 
 int main(int argc, char **argv) {
-    (void)argc;
-    (void)argv;
+	if (argc < 2 || argc > 3) {
+		std::cout << "usage: ./webserv [config] [log level]\n";
+		return 1;
+	}
+
+	std::string level = (argc == 3) ? argv[2] : "INFO";
+
+	if (!initLogger(level))
+		return 1;
+	Logger::get().setLogFile("webserv.log");
+
+	LOG_DEBUG() << "test log debug";
+	LOG_INFO() << "test log info";
+	LOG_WARNING() << "test log warning";
+	LOG_ERROR() << "test log warning";
 
     return 0;
 }
