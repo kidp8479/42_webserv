441c895a48a97090c4bfae5c4d3528ff61f8a197
pafroidu <pafroidu@student.42.fr>
2026-04-08 10:46:11 +0200

    fix: fix merged main after conflicts resolution

--- main.cpp
+++ main.cpp
@@ -2,29 +2,29 @@
 #include "logger/Logger.hpp"
 
 int main(int argc, char** argv) {
-    if (argc < 2 || argc > 3) {
+    if (argc > 3) {
         std::cout << "usage: ./webserv <config file> [log level]\n";
-        return 1;
+        return EXIT_FAILURE;
     }
 
     std::string level = (argc == 3) ? argv[2] : "INFO";
 
     if (!initLogger(level))
-        return 1;
+        return EXIT_FAILURE;
     Logger::get().setLogFile("webserv.log");
 
     LOG_DEBUG() << "test log debug";
     LOG_INFO() << "test log info";
     LOG_WARNING() << "test log warning";
     LOG_ERROR() << "test log error";
-    int main(int argc, char** argv) {
-        std::string path = (argc > 1) ? argv[1] : "conf/default.conf";
 
-        try {
-            ConfigTokenizer tokenizer(path);
-        } catch (const std::exception& e) {
-            std::cerr << e.what() << '\n';
-        }
+    std::string path = (argc > 1) ? argv[1] : "conf/default.conf";
 
-        return 0;
+    try {
+        ConfigTokenizer tokenizer(path);
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << '\n';
     }
+
+    return EXIT_SUCCESS;
+}
