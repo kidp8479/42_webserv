7ae5c49ebec6ea4ee830745dd73f4d495a6ae1de
d2 <d2.de.sousa@gmail.com>
2026-04-15 12:12:11 +0200

    chore(Fd) add comments for default arguments in ctor

--- server/Fd.hpp
+++ server/Fd.hpp
@@ -5,6 +5,11 @@
 
 class Fd {
 public:
+	// this is a ctor with a default argument
+	// at construction time if no fd is specified it is defaulted to -1
+	// ex: Fd a;  fd = -1
+	// Fd b(5); fd = 5
+	// note: default arguments belong in declaration
 	explicit Fd(int fd = -1);
 	~Fd();
 
