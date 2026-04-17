6bb352d16031dc83e66f3326019aa681ef9ac41c
d2 <d2.de.sousa@gmail.com>
2026-04-17 16:38:58 +0200

    tests(fd) add reset test suite

--- tests/server/test_fd.cpp
+++ tests/server/test_fd.cpp
@@ -55,6 +55,75 @@ TEST(FdRAII, ReleaseTransfersOwnership)
 	EXPECT_NE(-1, result);
 }
 
+TEST(FdReset, ResetFromValidClosesOldFd)
+{
+	int fd1 = open("/tmp/test_fd1.txt", O_CREAT | O_RDWR, 0644);
+	int fd2 = open("/tmp/test_fd2.txt", O_CREAT | O_RDWR, 0644);
+
+	ASSERT_NE(fd1, -1);
+	ASSERT_NE(fd2, -1);
+
+	{
+		Fd wrapper(fd1);
+		ASSERT_TRUE(wrapper.valid());
+
+		wrapper.reset(fd2);
+
+		EXPECT_EQ(wrapper.getFd(), fd2);
+
+		// old fd must be closed
+		EXPECT_EQ(fcntl(fd1, F_GETFD), -1);
+	}
+
+	// wrapper should still manage fd2 and close it now
+	EXPECT_EQ(fcntl(fd2, F_GETFD), -1);
+}
+
+TEST(FdReset, ResetFromInvalidSetsNewFd)
+{
+	int fd = open("/tmp/test_fd.txt", O_CREAT | O_RDWR, 0644);
+	ASSERT_NE(fd, -1);
+
+	Fd wrapper; // invalid fd (-1)
+	ASSERT_FALSE(wrapper.valid());
+
+	wrapper.reset(fd);
+
+	EXPECT_TRUE(wrapper.valid());
+	EXPECT_EQ(wrapper.getFd(), fd);
+
+	// cleanup check
+	EXPECT_NE(fcntl(fd, F_GETFD), -1);
+}
+
+TEST(FdReset, ResetMultipleTimesNoLeaks)
+{
+	int fd1 = open("/tmp/test_fd1.txt", O_CREAT | O_RDWR, 0644);
+	int fd2 = open("/tmp/test_fd2.txt", O_CREAT | O_RDWR, 0644);
+	int fd3 = open("/tmp/test_fd3.txt", O_CREAT | O_RDWR, 0644);
+
+	ASSERT_NE(fd1, -1);
+	ASSERT_NE(fd2, -1);
+	ASSERT_NE(fd3, -1);
+
+	{
+		Fd wrapper(fd1);
+
+		wrapper.reset(fd2);
+		EXPECT_EQ(wrapper.getFd(), fd2);
+
+		wrapper.reset(fd3);
+		EXPECT_EQ(wrapper.getFd(), fd3);
+
+		// intermediate fds should be closed
+		EXPECT_EQ(fcntl(fd1, F_GETFD), -1);
+		EXPECT_EQ(fcntl(fd2, F_GETFD), -1);
+	}
+
+	// final fd should also be closed by destructor
+	EXPECT_EQ(fcntl(fd3, F_GETFD), -1);
+}
+
 TEST(FdRAII, ReleaseSetsInternalFdToMinusOne)
 {
 	int fd = open("/tmp/test_fd.txt", O_CREAT | O_RDONLY, 0644);
