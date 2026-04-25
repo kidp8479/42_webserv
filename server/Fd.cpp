1d81c7ac40d7fade006c8b53a5451ab812ec63e1
d2 <d2.de.sousa@gmail.com>
2026-04-25 14:05:32 +0200

    chore(fd) comment out debug logs to avoid busy wait spamming

--- server/Fd.cpp
+++ server/Fd.cpp
@@ -12,7 +12,7 @@
  * @param[in] fd File descriptor to manage (-1 means invalid)
  */
 Fd::Fd(int fd) : fd_(fd) {
-    LOG_DEBUG() << "Fd created: " << fd_;
+    //LOG_DEBUG() << "Fd created: " << fd_;
 }
 
 /**
@@ -22,7 +22,7 @@ Fd::Fd(int fd) : fd_(fd) {
  */
 Fd::~Fd() {
     if (fd_ >= 0) {
-        LOG_DEBUG() << "Fd closed: " << fd_;
+    //  LOG_DEBUG() << "Fd closed: " << fd_;
         close(fd_);
     }
 }
@@ -47,10 +47,10 @@ int Fd::getFd() const {
 void Fd::reset(int new_fd) {
     if (fd_ != new_fd) {
         if (fd_ >= 0) {
-            LOG_DEBUG() << "Fd reset: closing fd " << fd_;
+//          LOG_DEBUG() << "Fd reset: closing fd " << fd_;
             close(fd_);
         }
-        LOG_DEBUG() << "Fd now holds: fd " << new_fd;
+//        LOG_DEBUG() << "Fd now holds: fd " << new_fd;
         fd_ = new_fd;
     }
 }
@@ -64,7 +64,7 @@ void Fd::reset(int new_fd) {
 int Fd::release() {
     int tmp = fd_;
     fd_ = -1;
-    LOG_DEBUG() << "Fd released: " << tmp;
+//    LOG_DEBUG() << "Fd released: " << tmp;
     return tmp;
 }
 
