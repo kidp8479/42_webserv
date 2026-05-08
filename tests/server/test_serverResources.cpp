54ef613531e24d9604f31d9a3254766d96a2fbf8
d2 <d2.de.sousa@gmail.com>
2026-05-08 17:11:50 +0200

    tests(core) add ServeResources unit testing

---
+++ tests/server/test_serverResources.cpp
@@ -0,0 +1,41 @@
+#include <gtest/gtest.h>
+#include "../../core/ServerResources.hpp"
+#include "../../config/ServerConfig.hpp"
+#include "../../handlers/Router.hpp"
+
+ServerConfig createDummyServerConfig(int port = 8083) {
+	ServerConfig sconf;
+	sconf.setPort(port);
+
+	LocationConfig loc;
+	loc.setPath("/");
+
+	sconf.addLocationBlock(loc);
+
+	return sconf;
+}
+
+TEST(ServerResources, Constructor_StoresServerConfig) {
+	ServerConfig sconf = createDummyServerConfig(8080);
+
+	ServerResources resources(sconf);
+
+	EXPECT_EQ(resources.serverConfig().getPort(), 8080);
+}
+
+TEST(ServerResources, Constructor_InitializesRouter) {
+	ServerConfig sconf = createDummyServerConfig();
+
+	ServerResources resources(sconf);
+
+	EXPECT_NO_THROW(resources.router());
+}
+
+TEST(ServerResources, CopyConstructor_RebindsRouterCorrectly) {
+	ServerConfig sconf = createDummyServerConfig();
+	ServerResources original(sconf);
+	ServerResources copy(original);
+
+	EXPECT_NO_THROW(copy.router());
+
+}
