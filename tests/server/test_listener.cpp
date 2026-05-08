dfabc48aae627ca63761a3b7cb96eba43c02e12e
d2 <d2.de.sousa@gmail.com>
2026-05-08 18:15:35 +0200

    tests(Listener) add unit test setup for listener

---
+++ tests/server/test_listener.cpp
@@ -0,0 +1,39 @@
+#include <gtest/gtest.h>
+//#include "../../core/ServerResources.hpp"
+#include "../../core/Listener.hpp"
+#include "../../config/ServerConfig.hpp"
+#include "../../core/EventLoop.hpp"
+#include "../../core/ServerResources.hpp"
+//#include "../../handlers/Router.hpp"
+
+ServerConfig createDummyServerConfig(int port = 8083) {
+	ServerConfig sconf;
+	sconf.setPort(port);
+
+	LocationConfig loc;
+	loc.setPath("/");
+
+	sconf.addLocationBlock(loc);
+
+	return sconf;
+}
+
+class ListenerTestFixture : public ::testing::Test {
+protected:
+	ServerConfig server_config;
+	ServerResources resources;
+	EventLoop loop;
+
+	ListenerTestFixture() :
+		server_config(createDummyServerConfig(8088)),
+		resources(server_config),
+		loop()
+	{}
+};
+
+TEST_F(ListenerTestFixture, Constructor_CreatesValidSocket) {
+	Listener* listener = new Listener(8088, loop, resources);
+
+	EXPECT_GT(listener->getFd(), 0);
+	EXPECT_STREQ(listener->name(), "Listener");
+}
