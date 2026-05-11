0274a9a781a09e9bdae54f23f6e57f959dc25c39
pafroidu <pafroidu@student.42.fr>
2026-05-11 12:49:58 +0200

    feat: beggining of run() implementation - save

--- handlers/Handler.cpp
+++ handlers/Handler.cpp
@@ -4,10 +4,19 @@
  * @brief Stub: returns static hello world response.
  * @note Will be replaced by Pauline's full handler implementation.
  */
-void Handler::run(const Request& request, const LocationConfig& loc,
-                  Response& response) {
-    (void)request;
-    (void)loc;
+void Handler::run(const Request& request, const LocationConfig& location,
+                  const ServerConfig& server, Response& response) {
+    // this is called "aggregate initialization" - init members in the order of
+    // declaration
+    HandlerContext handler_context = {request, location, server, response};
+
+    // if request parsing has marked isError true, return corresponding error
+    // code
+    if (request.isError()) {
+        sendError(request.getErrorCode(), handler_context);
+        return;
+    }
+
     // stub: same hello world as before, keeps server testable
     response.setRaw(
         "HTTP/1.1 200 OK\r\n"
