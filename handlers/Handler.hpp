a62f3e608a0cc2b1836b5eea1401f7e09772650c
pafroidu <pafroidu@student.42.fr>
2026-05-11 10:30:21 +0200

    feat: write Handler hpp

--- handlers/Handler.hpp
+++ handlers/Handler.hpp
@@ -2,13 +2,36 @@
 #define HANDLER_HPP
 
 #include "../config/LocationConfig.hpp"
+#include "../config/ServerConfig.hpp"
 #include "../http/Request.hpp"
 #include "../http/Response.hpp"
 
 class Handler {
 public:
-    static void run(const Request& request, const LocationConfig& loc,
-                    Response& response);
+    static void run(const Request& request, const LocationConfig& location,
+                    const ServerConfig& server, Response& response);
+
+private:
+    Handler();
+    ~Handler();
+    Handler(const Handler& copy);
+    Handler& operator=(const Handler& other);
+
+    static void handleReturn(const Request& request,
+                             const LocationConfig& location,
+                             const ServerConfig& server, Response& response);
+    static void handleCgiInterpreters(const Request& request,
+                                      const LocationConfig& location,
+                                      const ServerConfig& server,
+                                      Response& response);
+    static void handleUpload(const Request& request,
+                             const LocationConfig& location,
+                             const ServerConfig& server, Response& response);
+    static void handleStatic(const Request& request,
+                             const LocationConfig& location,
+                             const ServerConfig& server, Response& response);
+    static void sendError(int code, const ServerConfig& server,
+                          Response& response);
 };
 
 #endif
