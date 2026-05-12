daed5e1e8a067b60074e7607ae85373ffe570f1c
d2 <d2.de.sousa@gmail.com>
2026-05-12 10:46:30 +0200

    test(client) add test file for test_client

---
+++ tests/server/test_client.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest/h>
+#include "../../core/Listener.hpp"
+#include "../../core/Client.hpp"
+#include "../../core/EventLoop"
+#include "../../core/ServerConfig.hpp"
+#include "../../core/ServerResources.hpp"
+
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
