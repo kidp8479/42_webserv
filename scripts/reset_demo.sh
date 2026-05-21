03989ae297543e6edf8830bc028c1bb13be0ff85
pafroidu <pafroidu@student.42.fr>
2026-05-21 16:34:14 +0200

    fix: last fix to have matching behavior between default and demo behavior tests

--- scripts/reset_demo.sh
+++ scripts/reset_demo.sh
@@ -2,10 +2,10 @@
 find www/demo/upload/received -type f ! -name ".gitkeep" -delete
 find www/default/upload/received -type f ! -name ".gitkeep" -delete
 find www/demo/delete/files -type f ! -name ".gitkeep" -delete
-find www/default/delete -type f ! -name ".gitkeep" -delete
+find www/default/delete/files -type f ! -name ".gitkeep" -delete
 echo "hello from webserv demo" > www/demo/delete/files/file1.txt
 echo "delete me" > www/demo/delete/files/file2.txt
 echo "another test file" > www/demo/delete/files/file3.txt
-echo "hello from webserv default" > www/default/delete/file1.txt
-echo "delete me" > www/default/delete/file2.txt
+echo "hello from webserv default" > www/default/delete/files/file1.txt
+echo "delete me" > www/default/delete/files/file2.txt
 echo "demo files reset"
